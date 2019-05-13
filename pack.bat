3a58078e2b7ba5198a19b0cca7273465f0b947e7
Yiyang Hao <haoyiyang@nnthink.com>
2019-05-13 14:00:42 +0800

    automatically merge

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
 call git checkout master && call vsce package && call node packing/changename.js
 
-call git checkout enterprise && call vsce package && call node packing/changename.js
+call git checkout enterprise && call git merge master && call vsce package && call node packing/changename.js
 
 call git checkout master
