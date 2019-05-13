a3af7959d0126d128a58cb077129ae683c59faf5
Yiyang Hao <haoyiyang@nnthink.com>
2019-05-13 14:25:01 +0800

    aa

--- pack.bat
+++ pack.bat
@@ -1,5 +1,7 @@
-call git checkout master && call vsce package && call node packing/changename.js
+call git checkout master && call vsce package -o community.vsix
 
-call git checkout enterprise && call git merge master && call vsce package && call node packing/changename.js
+call git checkout enterprise && call git merge master && call vsce package -o enterprise.vsix
 
 call git checkout master
+
+call node packing/changename.js
--- packing/changename.js
+++ packing/changename.js
@@ -2,4 +2,5 @@ var fs = require("fs");
 var path = require("path");
 
 var packageJson = require("../package.json");
-fs.renameSync(path.join(__dirname, "..", packageJson.name + "-" + packageJson.version + ".vsix"), path.join(__dirname, "..", packageJson.name + "-" + packageJson.version + "-community.vsix"))
+fs.renameSync(path.join(__dirname, "..", "community.vsix"), path.join(__dirname, "..", packageJson.name + "-" + packageJson.version + "-community.vsix"))
+fs.renameSync(path.join(__dirname, "..", "enterprise.vsix"), path.join(__dirname, "..", packageJson.name + "-" + packageJson.version + "-enterprise.vsix"))
