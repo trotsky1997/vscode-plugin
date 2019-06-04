2e0cf4c8acc3e0bb04b9c81098fac308b36e5ff2
Yiyang Hao <haoyiyang@nnthink.com>
2019-06-04 17:14:59 +0800

    multiple import rescues

--- src/lang/java.ts
+++ src/lang/java.ts
@@ -92,7 +92,7 @@ export class JavaLangUtil extends LangUtil {
     public rescue(document: vscode.TextDocument, rescues: Rescue[]) {
         const editor = vscode.window.activeTextEditor;
         let imports: Array<[string, number]> | null = null;
-        let importStart = 0;
+        let importStart = -1;
         function prepareImports() {
             imports = [];
             for (let i = 0; i < document.lineCount; i++) {
@@ -107,39 +107,44 @@ export class JavaLangUtil extends LangUtil {
                 }
             }
         }
-        function rescueImport(rescue: Rescue) {
-            if (imports === null) {
-                prepareImports();
-            }
-            let prevImport: [string, number] = ["", importStart];
-            for (const importContent of imports) {
-                const compareResult = importContent[0].localeCompare(rescue.value);
-                if (compareResult > 0) {
-                    // stop here
-                    editor.edit((editBuilder) => {
-                        editBuilder.insert(new vscode.Position(prevImport[1], 0), `import ${rescue.value};\n`);
-                    });
-                    return;
-                }
-                prevImport = importContent;
-            }
-            editor.edit((editBuilder) => {
-                editBuilder.insert(new vscode.Position(prevImport[1], 0), `import ${rescue.value};\n`);
-            });
-        }
         if (editor) {
-            for (const rescue of rescues) {
-                switch (rescue.type) {
-                    case "import":
-                        if (vscode.workspace.getConfiguration().get("aiXcoder.autoImport")) {
-                            rescueImport(rescue);
+            editor.edit((editBuilder) => {
+                function rescueImport(rescue: Rescue) {
+                    if (imports === null) {
+                        prepareImports();
+                    }
+                    let prevImport: [string, number] = ["", importStart];
+                    for (let i = 0; i < imports.length; i++) {
+                        const importContent = imports[i];
+                        const compareResult = importContent[0].localeCompare(rescue.value);
+                        if (compareResult > 0) {
+                            // stop here
+                            imports.splice(i, 0, [rescue.value, prevImport[1] + 1]);
+                            editBuilder.insert(new vscode.Position(prevImport[1] + 1, 0), `import ${rescue.value};\n`);
+                            for (i += 1; i < imports.length; i++) {
+                                imports[i][1]++;
+                            }
+                            return;
                         }
-                        break;
-                    default:
-                        logger.warn(`Unknown rescue type ${rescue.type} with value=${rescue.value}`);
-                        break;
+                        prevImport = importContent;
+                    }
+                    imports.push([rescue.value, prevImport[1] + 1]);
+                    editBuilder.insert(new vscode.Position(prevImport[1] + 1, 0), `import ${rescue.value};\n`);
                 }
-            }
+
+                for (const rescue of rescues) {
+                    switch (rescue.type) {
+                        case "import":
+                            if (vscode.workspace.getConfiguration().get("aiXcoder.autoImport")) {
+                                rescueImport(rescue);
+                            }
+                            break;
+                        default:
+                            logger.warn(`Unknown rescue type ${rescue.type} with value=${rescue.value}`);
+                            break;
+                    }
+                }
+            };
         }
     }
 }
