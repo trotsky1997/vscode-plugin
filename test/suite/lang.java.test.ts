5336db170ff3a5703e810c4945f3118fe6a50217
Yiyang Hao <haoyiyang@nnthink.com>
2019-09-16 14:53:51 +0800

    test suite 2

--- test/suite/lang.java.test.ts
+++ test/suite/lang.java.test.ts
@@ -1,9 +1,10 @@
-import * as assert from "assert";
+import { expect } from "chai";
 import { after } from "mocha";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import { getInstance } from "../../src/lang/commons";
 // import * as myExtension from '../extension';
 
 suite("Extension Test Suite", () => {
@@ -11,8 +12,49 @@ suite("Extension Test Suite", () => {
     vscode.window.showInformationMessage("All tests done!");
   });
 
-  test("Sample test", () => {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
-  });
+  const sourceTokens = `import java . util . regex . Matcher ;
+  import java . util . regex . Pattern ;
+  public class RegexMatches {
+  public static void main ( String args [] ) {
+  String line = <str> ;
+  String pattern = <str> ;
+  Pattern r = Pattern . compile ( pattern ) ;
+  Matcher m = r . matcher ( line ) ;
+  if ( m . find ( ) ) {
+  System . out . println ( <str> + m . group ( <int>0 ) ) ;
+  System . out . println ( <str> + m . group ( <int>1 ) ) ;
+  System . out . println ( <str> + m . group ( <int> ) ) ;
+  } else {
+  System . out . println ( <str> ) ;
+  }
+  }
+  }`;
+  const sourceExpect = `import java.util.regex.Matcher;
+  import java.util.regex.Pattern;
+  public class RegexMatches {
+      public static void main(String args[]) {
+          String line = "";
+          String pattern = "";
+          Pattern r = Pattern.compile(pattern);
+          Matcher m = r.matcher(line);
+          if (m.find()) {
+              System.out.println("Found value: " + m.group(0));
+              System.out.println("Found value: " + m.group(1));
+              System.out.println("Found value: " + m.group(0));
+          } else {
+              System.out.println("");
+          }
+      }
+  }`;
+
+  const langUtil = getInstance("java");
+  const sourceTokensLines = sourceTokens.split("\n").map((_) => _.trim());
+  const sourceExpectLines = sourceExpect.split("\n").map((_) => _.trim());
+  expect(sourceTokensLines.length).to.equal(sourceExpectLines.length);
+  for (let i = 0; i < sourceTokensLines.length; i++) {
+    const tokens = sourceTokensLines[i].split(" ");
+    const expectedValue = sourceExpectLines[i];
+    const rendered = langUtil.render(tokens, 0);
+    expect(rendered).to.equal(expectedValue);
+  }
 });
